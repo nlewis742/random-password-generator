ce68b0ac3d77e2038e41a3e456f5e194ac5b6c49
Natasha <nlackas12@gmail.com>
2023-01-17 09:19:03 -0600

    added css and readme

---
+++ Assets/script.js
@@ -0,0 +1,26 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+const possibilities = {
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowerCase: "abcdefghijklmnopqrstuvwxyz",
+  number: "0123456789",
+  symbol: "!@#$%^&*()_+~`|}{[]:;?><,./-="
+}
+
+let password = "";
+let tempPossibilities = "";
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
