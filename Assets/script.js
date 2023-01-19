5d7d163056ae01ce0b9d235d1353a56aaeac1d19
Natasha <nlackas12@gmail.com>
2023-01-18 19:00:08 -0600

    fixed location of randomization

--- Assets/script.js
+++ Assets/script.js
@@ -85,22 +85,27 @@ function generatePassword() {
   }
 
   return password;
+
  
 }
 
-console.log(password);
 
-  // Shuffles through the password variable to randomize further 
-  password = password.split('').sort(function(){return 0.5-Math.random()}).join('');
 
-  console.log(password);
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  console.log(password);
   var passwordText = document.querySelector("#password");
-
+  // Shuffles through the password variable to randomize further 
+  password = password.split('').sort(function(){return 0.5-Math.random()}).join('');
   passwordText.value = password;
+  console.log(password);
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
